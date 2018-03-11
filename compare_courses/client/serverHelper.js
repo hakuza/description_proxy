8c91f55a01e0c3c33cdb370aa162d44e5e1cb73c
James Davidson <jfdavidson10@gmail.com>
2018-03-10 16:11:25 -0800

    deleted unneeded folders

--- compare_courses/client/serverHelper.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-exports.get = function (courseID) {
-  return axios.get('/api/compare', {
-    params: {
-      id: courseID
-    }
-  });
-};
--- compare_courses/server/routes.js
+++
@@ -1,6 +0,0 @@
-const controller = require('./controllers');
-const router = require('express').Router();
-
-router.get('/compare', controller.compare.get);
-
-module.exports = router;
